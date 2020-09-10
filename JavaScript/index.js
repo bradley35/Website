99a6cd2d4c20f523cd0f92f4327386546ff0299b
Bradley <bamay@bamapp.net>
2020-09-10 01:03:22 -0400

    Updated for 2020

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,6 @@
 $(document).scroll(function(location) {
   //console.log($(window).scrollTop())
-  if ($(window).scrollTop() > 0) {
+  if ($(window).scrollTop() > 10) {
     $("bar").removeClass('undoShadow');
     $("bar").addClass('goShadow');
   } else {
@@ -35,10 +35,10 @@ $(document).ready(function() {
                 section.contents.push({icon: respository.links.avatar.href, link: respository.links.html.href, title: respository.name, description: respository.description})
               }else{
 
-              theSection.appending.forEach(function(item){
-                section.contents.push({icon: item.icon, link: item.link, title: item.title, date: item.date, description: item.description})
-              })
-            }
+                theSection.appending.forEach(function(item){
+                  section.contents.push({icon: item.icon, link: item.link, title: item.title, date: item.date, description: item.description})
+                })
+              }
               sections[sectionName] = section
               reloadView()
 
@@ -68,58 +68,59 @@ $(document).ready(function() {
 
 
 
-      }, error: function(){
-        var sectionName = "Open Source Projects"
-        var section;
-        if(!sections[sectionName]){
-        section = {name: sectionName, contents: []}
-        }else{
-        section = section.sections[sectionName]
+        }, error: function(){
+          var sectionName = "Open Source Projects"
+          var section;
+          if(!sections[sectionName]){
+            section = {name: sectionName, contents: []}
+          }else{
+            section = section.sections[sectionName]
+          }
+          console.log(sectionName)
+          console.log("CAT")
+          section.contents.push({icon: respository.links.avatar.href, link: respository.links.html.href, title: respository.name, description: respository.description})
+          sections[sectionName] = section
+          reloadView()
+        }})})}});
+
+
+
+
+        function reloadView(){
+          var html = "";
+          for(var sectionName in sections){
+            var section = sections[sectionName]
+            var title = section.name;
+
+
+            html = html + ('<h1 align="center" style="color: #606060;">' + title + '</h1>\n<line></line>');
+            html = html + ('<table>');
+            var count = section.contents.length;
+            var i = 0;
+            section.contents.forEach(function(app) {
+              i++;
+
+              html = html + ('<tr>\n<th style="text-align:initial"><a href="');
+              if (app.link) {
+                html = html + app.link;
+              }
+              html = html + '"><img class="img" src="' + app.icon + '"></img></a></th>\n<th><text>\n<h4>' + app.title + '</h4>\n<p>' + app.description + '</p>';
+              if(app.date){
+                html = html + '<date>'+app.date+'</date>';
+              }
+              html = html + '\n</text></th>\n</tr>';
+
+
+              if (i < (count)) {
+                html = html + "<tr style='padding-top:10px; padding-bottom: 10px; display:table-cell;'><th><thinline></thinline></th></tr>";
+              }
+            })
+            html = html + '</table>';
+            console.log("CHEESE")
+
+
+          }
+          $("#contents").html(html);
         }
-        console.log(sectionName)
-        console.log("CAT")
-        section.contents.push({icon: respository.links.avatar.href, link: respository.links.html.href, title: respository.name, description: respository.description})
-        sections[sectionName] = section
-        reloadView()
-      }})})}})
 
-
-
-
-  function reloadView(){
-      var html = "";
-    for(var sectionName in sections){
-      var section = sections[sectionName]
-        var title = section.name;
-
-
-        html = html + ('<h1 align="center" style="color: #606060;">' + title + '</h1>\n<line></line>');
-        html = html + ('<table>');
-        var count = section.contents.length;
-        var i = 0;
-        section.contents.forEach(function(app) {
-          i++;
-
-          html = html + ('<tr>\n<th style="text-align:initial"><a href="');
-          if (app.link) {
-            html = html + app.link;
-          }
-          html = html + '"><img src="' + app.icon + '"></img></a></th>\n<th><text>\n<h4>' + app.title + '</h4>\n<p>' + app.description + '</p>';
-          if(app.date){
-            html = html + '<date>'+app.date+'</date>';
-          }
-          html = html + '\n</text></th>\n</tr>';
-
-
-          if (i < (count)) {
-            html = html + "<tr style='padding-top:10px; padding-bottom: 10px; display:table-cell;'><th><thinline></thinline></th></tr>";
-          }
-        })
-        html = html + '</table>';
-        console.log("CHEESE")
-
-
-    }
-    $("#contents").html(html);
-  }
-})
+      });
